6bfb6bd65a42d695340546948cad687bbd1fbc6e
AR Assignments <abdurrehman905623@gmail.com>
2025-11-06 04:49:36 +0800

    uploead

--- Frontend-Angular/src/app/app.routes.ts
+++ Frontend-Angular/src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { Home } from './home/home';
+import { About } from './about/about';
+import { Contact } from './contact/contact';
+import { App } from './app';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:"",component:Home},
+    {path:"home",component:Home},
+    {path:"about",component:About},
+    {path:"contact",component:Contact},
+];
